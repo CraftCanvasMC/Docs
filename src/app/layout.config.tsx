ce4335611bdbfab8a5c09c95e8f4585e98d40214
Szymon Pel <szymonpel@proton.me>
2025-02-16 08:48:51 +0100

    fix: remove unnecessary margin from title

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
-      <div className='flex items-center gap-2 md:mb-1.5'>
+      <div className='flex items-center gap-2'>
         <Image src='/logo.png' alt='CanvasMC Logo' width={22} height={22} />
         <span>CanvasMC Docs</span>
       </div>
