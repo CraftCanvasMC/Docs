5e14e7542737463c7f7d480c4d0cfed160792b2f
yunuservices <yunuservices@gmail.com>
2025-10-12 21:07:50 +0300

    feat: tried to fix light/dark mode flickering. :(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
+// app/layout.tsx
+
 import './globals.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import type { Viewport } from 'next';
 import type { ReactNode } from 'react';
 import { geist, geistMono } from '~/lib/fonts';
 import ThemeToggle from './theme';
-import dynamic from "next/dynamic";
-import BackingClient from "./BackingClient";
+import BackingClient from './BackingClient';
 
 export const viewport: Viewport = {
   themeColor: '#2a2d30',
@@ -14,12 +15,14 @@ export const viewport: Viewport = {
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${geist.variable} ${geistMono.variable}`} suppressHydrationWarning>
+      
       <body className="flex min-h-screen flex-col">
-        <BackingClient />
-        <header className="flex justify-end p-4 relative z-10">
-          <ThemeToggle />
-        </header>
-        <RootProvider>
+        <RootProvider> 
+          <BackingClient />
+          <header className="flex justify-end p-4 relative z-10">
+            <ThemeToggle />
+          </header>
+          
           <div className="relative z-10">{children}</div>
         </RootProvider>
       </body>
