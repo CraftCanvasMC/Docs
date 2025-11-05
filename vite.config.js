61653e32688a597bd78a88ba68d81142f78fc3f9
Your Name <samdhi2323@gmail.com>
2025-11-05 23:41:35 +0530

    Ah

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,9 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3000,
-    strictPort: false,
-    hmr: {
-      protocol: "wss",
-      clientPort: 443,
-    },
   },
   preview: {
     host: "0.0.0.0",
     port: 3000,
-    strictPort: false,
   },
 });
