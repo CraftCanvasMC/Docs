2de9641e48254aa15a27a3676d813b3c330b62a5
Dueris <jedimastertoothless@hotmail.com>
2026-02-01 02:18:45 -0800

    Horizon documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,103 +1,112 @@
-import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
-import Icons from "starlight-plugin-icons";
+import starlight from "@astrojs/starlight";
+import svelte from "@astrojs/svelte";
 import UnoCSS from "unocss/astro";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 export default defineConfig({
   site: "https://docs.canvasmc.io",
+
   integrations: [
     svelte(),
     UnoCSS(),
-    Icons({
-      sidebar: true,
-      extractSafelist: true,
-      starlight: {
-        title: "CanvasMC Docs",
-        social: [
-          { icon: "github", label: "GitHub", href: "https://github.com/CraftCanvasMC" },
-          { icon: "discord", label: "Discord", href: "https://canvasmc.io/discord" },
-        ],
-        components: {
-          SocialIcons: "./src/components/overrides/SocialIcons.astro",
-          Footer: "./src/components/overrides/Footer.astro",
-          LastUpdated: "./src/components/overrides/LastUpdated.astro",
-        },
-        sidebar: [
+
+    starlight({
+      title: "CanvasMC Docs",
+
+      customCss: [
+        "@fontsource/jetbrains-mono/400.css",
+        "@fontsource/jetbrains-mono/600.css",
+        "@fontsource/poppins/400.css",
+        "@fontsource/poppins/600.css",
+        "./src/styles/globals.css",
+      ],
+
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/CraftCanvasMC" },
+        { icon: "discord", label: "Discord", href: "https://canvasmc.io/discord" },
+      ],
+
+      plugins: [
+        starlightSidebarTopics(
+          [
+            {
+              label: "Horizon",
+              link: "/horizon/introduction/",
+              items: [
+                "horizon/introduction",
+                {
+                  label: "Information",
+                  items: [
+                    "horizon/info/breaks",
+                    "horizon/info/installing"
+                  ]
+                },
+                {
+                  label: "Development",
+                  items: [
+                    "horizon/dev/intro",
+                    "horizon/dev/classloader",
+                    "horizon/dev/objecttree",
+                    "horizon/dev/mixins",
+                    "horizon/dev/api"
+                  ]
+                }
+              ]
+            },
+            {
+              label: "Canvas",
+              link: "/canvas/introduction/",
+              items: [
+                "canvas/introduction",
+                {
+                  label: "Guides",
+                  items: [
+                    {
+                      label: "Getting Started",
+                      items: [
+                        "canvas/guides/getting-started/installation",
+                        "canvas/guides/getting-started/plugins",
+                      ],
+                    },
+                    {
+                      label: "Developers",
+                      items: [
+                        "canvas/guides/developers/contributing/canvas",
+                        "canvas/guides/developers/contributing/docs",
+                        "canvas/guides/developers/rest-api"
+                      ],
+                    },
+                  ],
+                },
+                {
+                  label: "Info",
+                  items: [
+                    "canvas/info/helpful/compat",
+                    "canvas/info/helpful/lts",
+                    {
+                      label: "Folia",
+                      items: [
+                        "canvas/info/folia/overview",
+                        "canvas/info/folia/fixes",
+                        "canvas/info/folia/region-logic",
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
           {
-            label: "Introduction",
-            link: "/introduction",
-            icon: "i-ic:baseline-book",
-          },
-          {
-            label: "Getting Started",
-            items: [
-              {
-                icon: "i-ic:outline-rocket-launch",
-                label: "Installation",
-                slug: "guides/getting-started/installation",
-              },
-              {
-                icon: "i-ic:baseline-install-desktop",
-                label: "Installing Plugins",
-                slug: "guides/getting-started/plugins",
-              },
-            ],
-          },
-          {
-            label: "Helpful",
-            items: [
-              { icon: "i-ri:java-line", label: "Plugin Compatibility", slug: "info/helpful/compat" },
-              { icon: "i-ic:twotone-receipt-long", label: "Long Term Support", slug: "info/helpful/lts" },
-            ],
-          },
-          {
-            label: "For Developers",
-            items: [
-              { icon: "i-ic:outline-question-answer", label: "REST API", slug: "guides/developers/rest-api" },
-              {
-                label: "Contributing",
-                items: [
-                  {
-                    icon: "i-ic:outline-code",
-                    label: "Contributing to Canvas",
-                    slug: "guides/developers/contributing/canvas",
-                  },
-                  { icon: "i-mdi:github", label: "Contributing to Docs", slug: "guides/developers/contributing/docs" },
-                ],
-              },
-            ],
-          },
-          {
-            label: "Folia",
-            items: [
-              { icon: "i-streamline-plump:feather-pen-remix", label: "Overview", slug: "info/folia/overview" },
-              { icon: "i-ic:outline-difference", label: "Fixes to Folia", slug: "info/folia/fixes" },
-              {
-                icon: "i-carbon:cics-region-routing",
-                label: "Region Threading Logic",
-                slug: "info/folia/region-logic",
-              },
-            ],
-          },
-        ],
-        logo: {
-          src: "/public/logo.png",
-          alt: "CraftCanvasMC Logo",
-          href: "/",
-        },
-        lastUpdated: true,
-        editLink: {
-          baseUrl: "https://github.com/CraftCanvasMC/Docs/edit/main/",
-        },
-        customCss: [
-          "@fontsource/poppins/400.css",
-          "@fontsource/jetbrains-mono/400.css",
-          "@fontsource/jetbrains-mono/600.css",
-          "./src/styles/globals.css",
-        ],
-      },
+            topics: {
+              canvas: ["/canvas"],
+            },
+          }
+        ),
+      ],
     }),
   ],
+
   server: {
     port: 80,
     host: true,
