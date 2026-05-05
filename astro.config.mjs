5dfeaf69e1fe6286287cd86c7649ed2db6c2304d
Toffikk <slodkitofik@gmail.com>
2026-05-06 00:08:16 +0200

    use correct logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,17 @@ export default defineConfig({
     starlight({
       title: "CanvasMC Docs",
 
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            href: "/logo.png",
+          },
+        },
+      ],
+
       customCss: [
         "@fontsource/jetbrains-mono/400.css",
         "@fontsource/jetbrains-mono/600.css",
@@ -38,9 +49,9 @@ export default defineConfig({
         { icon: "discord", label: "Discord", href: "https://canvasmc.io/discord" },
       ],
       lastUpdated: true,
-        editLink: {
-          baseUrl: "https://github.com/CraftCanvasMC/Docs/edit/main/",
-        },
+      editLink: {
+        baseUrl: "https://github.com/CraftCanvasMC/Docs/edit/main/",
+      },
       components: {
         Footer: "./src/components/overrides/Footer.astro",
         LastUpdated: "./src/components/overrides/LastUpdated.astro",
